9793277922aa9dfd180820712c6d92ea25428a38
MUGO BRIAN <mikanmuchiri@gmail.com>
2021-09-08 03:12:21 +0000

    Update form.php

--- form.php
+++ form.php
@@ -1,26 +1,27 @@
 <?php
 if(isset($_POST["submit"])) {
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-$email_from = $_POST['email'];
+  $email_from = $_POST['email'];
 
-$email_subject = 'New Form Submission';
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "Subject: $subject.\n".
-                "Message: $message.\n";
+  $email_subject = 'New Form Submission';
+  $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                  "Subject: $subject.\n".
+                  "Message: $message.\n";
 
-$to = "mikanmuchiri@gmail.com";
+  $to = "mikanmuchiri@gmail.com";
 
-$headers = "From: $email_from \r\n";
+  $headers = "From: $email_from \r\n";
 
-$headers .= "Reply-To: $visitor_email \r\n";
+  $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+  mail($to,$email_subject,$email_body,$headers);
 
-header("Location:contact.html");
+  header("Location:contact.html");
+  echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
 }
 ?>
