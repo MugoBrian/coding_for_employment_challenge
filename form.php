f02b829d692d04887ba66301880ab104b3e195bc
MUGO BRIAN <mikanmuchiri@gmail.com>
2022-03-23 12:49:31 +0000

    Update form.php

--- form.php
+++ form.php
@@ -17,7 +17,7 @@ if(isset($_POST["submit"])) {
 
   $headers = "From: $email_from \r\n";
 
-  $headers .= "Reply-To: $visitor_email \r\n";
+  $headers = "Reply-To: $visitor_email \r\n";
 
   mail($to,$email_subject,$email_body,$headers);
 
